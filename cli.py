e3bbbe224b9946544e7faacf0fd0e285db3089d2
Nigel Davis <ndavis10@uoguelph.ca>
2022-01-23 01:16:09 -0500

    made code style improvements and added additional error checking

--- cli.py
+++ cli.py
@@ -1,164 +1,149 @@
 import json
 
-#function if code name (etc CIS*1300) is not entered 
-def courseWithNoCode(data,code,weight,name,term):
+# function if course code (ex. CIS*1300) is not entered 
+def get_courses_without_code(data, weight, name, term):
     course_list = []
     for course_attr in data["courses"]:
-                for course_value in data["courses"][course_attr]:
-                    check = True
-                    if weight and course_value["weight"] != float(weight):
-                            continue
-                    if code and code[-4:] not in course_value["code"]:
-                            continue
-                    if name and name.lower() not in (course_value["name"]).lower():
-                            continue
-                    if(term):
-                        check2 = False
-                        for desc in course_value["terms"]:
-                            if term.lower() in desc.lower() and term != "":
-                                check2 = True
-                        if not check2:
-                            continue
-                    #final output
-                    if check and (weight == "" and code=="" and name=="" and term=="")==False :
-                        course_list.append(course_value["code"])
+        for course_value in data["courses"][course_attr]:
+
+            if weight and course_value["weight"] != float(weight):
+                continue
+            if name and name.lower() not in (course_value["name"]).lower():
+                continue
+            if term and term not in course_value["terms"]:
+                continue
+
+            course_list.append(course_value["code"])
     return course_list
 
-#function if code name (etc CIS*1300) is entered as the course level or section (ex: 1300 or cis)
-def courseWithOnlyNumAlpha(data,code,weight,name,term):
+
+# function if course code (ex. CIS*1300) is entered as the course level or section (ex. 1300 or cis)
+def get_courses_with_partial_code(data, code, weight, name, term):
     course_list = []
     for course_attr in data["courses"]:
-                    for course_value in data["courses"][course_attr]:
-                        check = True
-                        if(weight):
-                            if course_value["weight"] != float(weight):
-                                continue
-                        if code.lower() not in course_value["code"].lower():
-                                continue
-                        if(name):
-                            if name.lower() not in (course_value["name"]).lower():
-                                continue                        
-                        if(term):
-                            check2 = False
-                            for desc in course_value["terms"]:
-                                if term.lower() in desc.lower() and term != "":
-                                    check2 = True
-                            if not check2:
-                                continue
+        for course_value in data["courses"][course_attr]:
 
-                        #final output
-                        if check:
-                            course_list.append(course_value["code"])
+            if weight and course_value["weight"] != float(weight):
+                continue
+            if code not in course_value["code"].lower():
+                continue
+            if name and name not in course_value["name"].lower():
+                continue                        
+            if term and term not in course_value["terms"]:
+                continue
+
+            course_list.append(course_value["code"])
     return course_list
 
-#function if code name (etc CIS*1300) is entered in full
-def courseWithCode(data,code,weight,name,term):
-    course_list = []
-    for course_attr in data["courses"][code[:-4].replace("*","").lower()]:
-                    check = True
-                    if(weight):
-                        if course_attr["weight"] != float(weight):
-                            continue
-                    if(code):
-                        if code[-4:] not in course_attr["code"]:
-                           continue
-                    if(name):
-                        if name.lower() not in (course_attr["name"]).lower():
-                           continue
-                    if(term):
-                        check2 = False
-                        for p in course_attr["terms"]:
-                            if term.lower() in p.lower() and term != "":
-                                check2 = True
-                        if not check2:
-                            continue
 
-                    #final output
-                    if check:
-                        course_list.append(course_attr["code"])
+# function if code name (ex. CIS*1300) is entered in full
+def get_courses_with_code(data, code, weight, name, term):
+    course_list = []
+    course_attr = code[:code.index('*')].lower()
+    for course_value in data["courses"][course_attr]:
+
+        if weight and course_value["weight"] != float(weight):
+            continue
+        if code and code[-4:] not in course_value["code"].lower():
+            continue
+        if name and name not in course_value["name"].lower():
+            continue
+        if term and term not in course_value["terms"]:
+            continue
+
+        course_list.append(course_value["code"])
+        break # only ever going to be one course so no need to keep looking
     return course_list                    
 
-#function running comand line interface
+
+# function running comand line interface
 def cli(data):
-        final_course_list = [] # list to hold all matching courses
-        # User input
-        inputFlag = True
 
-        # Course name ("programming", "math", "chemistry")
-        while inputFlag:
-            name = input("Course name (hit enter to skip field): ")
-            if name.isalpha() or name == "":
-                inputFlag = False
-            else:
-                print("Error: name cannot contain numbers")
-        inputFlag = True
+    # Course name (ex. "programming", "math", "chemistry")
+    name = input("Course name (hit enter to skip field): ").strip().lower()
 
-        # Course code (CIS*1300 or 1300)
-        while inputFlag:
-            code = input("Course code (hit enter to skip field): ")
+    # Course code (ex. CIS*1300 or CIS or 1300)
+    inputFlag = True
+    while inputFlag:
+        code = input("Course code (hit enter to skip field): ").strip().lower()
+        if code == "":
             inputFlag = False
-        inputFlag = True
-
-        # Season/term (either S, F, or W)
-        while inputFlag:
-            term = input("Course season/term (hit enter to skip field): ").upper()
-            if term == 'S' or term == 'F' or term == 'W' or term == "":
-                inputFlag = False
-            else:
-                print("Valid seasons: S, W, or F")
-        inputFlag = True
-
-        # Weight/credit (must be a decimal value)
-        while inputFlag:
-            weight = input("Course weight (hit enter to skip field): ")
-            if len(weight) != 0:
-                try:
-                    float(weight)
-                    inputFlag = False
-                except ValueError:
-                    print("Valid course weight: 0.25, 0.5, 0.75, 1.0 etc")
-                    continue
+        elif "*" in code:
+            dept, num = code.split('*')
+            if dept not in data['courses'].keys() or len(num) != 4 or not num.isnumeric():
+                print("Not a valid course code")
             else:
                 inputFlag = False
-
-        #check for length of code name and if inputted for example cis*1300, cis1300, cis, 1300, or null 
-        # add returned list values to  master course list
-        if (code) and len(code) > 4:
-            final_course_list =  courseWithCode(data,code,weight,name,term)
-        elif (code.isnumeric()  and len(code) < 5) or (code.isalpha() and len(code) < 5):
-            final_course_list = courseWithOnlyNumAlpha(data,code,weight,name,term)
+        elif len(code) < 5 and not code.isnumeric() and code not in data['courses'].keys():
+            print("Not a valid course code")
+        elif code.isnumeric() and len(code) != 4:
+            print("Not a valid course code")
         else:
-            final_course_list = courseWithNoCode(data,code,weight,name,term)
+            inputFlag = False
 
-        #check length of list of found courses and print to user
-        if len(final_course_list) == 0:
-            print("\n\nNo Courses Found\n")
+    # Term (either S, F, or W)
+    inputFlag = True
+    while inputFlag:
+        term = input("Course season/term (hit enter to skip field): ").strip().upper()
+        if term == 'S' or term == 'F' or term == 'W' or term == "":
+            inputFlag = False
         else:
-            print("\n\nCourses Found:\n")
-            for x in range(len(final_course_list)):
-                print(str(final_course_list[x]) + '\n')
+            print("Valid seasons: S, W, or F")
 
-        #Menu ------------------------------------------------------
-        continueSearch = input("\n\nSearch again? [y/n] ")
-        if continueSearch.lower() == "n" or continueSearch.lower() == "no":
-            return False
+    # Course Weight (must be a decimal value)
+    inputFlag = True
+    while inputFlag:
+        weight = input("Course weight (hit enter to skip field): ").strip()
+        if len(weight) != 0:
+            try:
+                float(weight)
+                inputFlag = False
+            except ValueError:
+                print("Valid course weight: 0.25, 0.5, 0.75, 1.0 etc")
+                continue
         else:
-            return True
+            inputFlag = False
+
+    # check for length of code name and if inputted for example cis*1300, cis1300, cis, 1300, or null 
+    # get course list depending on user input
+    if code and len(code) > 4:
+        final_course_list = get_courses_with_code(data, code, weight, name, term)
+    elif code and len(code) < 5:
+        final_course_list = get_courses_with_partial_code(data, code, weight, name, term)
+    else:
+        final_course_list = get_courses_without_code(data, weight, name, term)
+
+    # check length of list of found courses and print to user
+    if len(final_course_list) == 0:
+        print("\nNo Courses Found.")
+    else:
+        print("\nCourses Found:")
+        for course in final_course_list:
+            print(course)
+
+    continueSearch = input("\nSearch again? [y/n] ")
+    if continueSearch.lower() == "n" or continueSearch.lower() == "no":
+        return False
+    return True
     
-#function to open JSON file
-def get_course_info():
-    file = open("course_info.json", "r") 
-    data = file.read()
-    coursedata = json.loads(data)
+
+# function to open JSON file
+def get_course_info(filename):
+    with open(filename, "r") as file:
+        data = file.read()
+        coursedata = json.loads(data)
     return coursedata
 
 
 def main():
-    course_info = get_course_info()
+    course_info = get_course_info("course_info.json")
     print("\nWelcome to UoG Course Search")
+
     while True:
         keep_going = cli(course_info)
         if not keep_going:
             break
 
-main()
+
+if __name__ == '__main__':
+    main()
