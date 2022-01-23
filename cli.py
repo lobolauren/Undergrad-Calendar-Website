c13112ff17fb332b5c46af08a45e738cb5e3c8ba
holmesg <holmesg@uoguelph.ca>
2022-01-23 14:28:08 -0500

    cli.py now prints more information about courses & has option to show description

--- cli.py
+++ cli.py
@@ -1,6 +1,8 @@
 import json
 
-# function if course code (ex. CIS*1300) is not entered 
+# function if course code (ex. CIS*1300) is not entered
+
+
 def get_courses_without_code(data, weight, name, term):
     course_list = []
     for course_attr in data["courses"]:
@@ -13,7 +15,7 @@ def get_courses_without_code(data, weight, name, term):
             if term and term not in course_value["terms"]:
                 continue
 
-            course_list.append(course_value["code"])
+            course_list.append(course_value)
     return course_list
 
 
@@ -28,11 +30,11 @@ def get_courses_with_partial_code(data, code, weight, name, term):
             if code not in course_value["code"].lower():
                 continue
             if name and name not in course_value["name"].lower():
-                continue                        
+                continue
             if term and term not in course_value["terms"]:
                 continue
 
-            course_list.append(course_value["code"])
+            course_list.append(course_value)
     return course_list
 
 
@@ -51,9 +53,9 @@ def get_courses_with_code(data, code, weight, name, term):
         if term and term not in course_value["terms"]:
             continue
 
-        course_list.append(course_value["code"])
-        break # only ever going to be one course so no need to keep looking
-    return course_list                    
+        course_list.append(course_value)
+        break  # only ever going to be one course so no need to keep looking
+    return course_list
 
 
 # function running comand line interface
@@ -65,28 +67,39 @@ def cli(data):
     # Course code (ex. CIS*1300 or CIS or 1300)
     inputFlag = True
     while inputFlag:
-        code = input("Course code/number (hit enter to skip field): ").strip().lower()
+        code = input(
+            "Course code/number (hit enter to skip field): ").strip().lower()
         if code == "":
             inputFlag = False
         elif "*" in code:
             dept, num = code.split('*')
             if dept not in data['courses'].keys() or len(num) != 4 or not num.isnumeric():
-                print("Not a valid course code. Format: [code]/[number]/[code]*[number] (ex. cis or 1000 or cis*1000)")
+                print(
+                    "Not a valid course code. Format: [code]/[number]/[code]*[number] (ex. cis or 1000 or cis*1000)")
             else:
                 inputFlag = False
         elif len(code) > 4:
+            
             print("Not a valid course code. Format: [code]/[number]/[code]*[number] (ex. cis or 1000 or cis*1000)")
+
         elif len(code) < 5 and not code.isnumeric() and code not in data['courses'].keys():
+
             print("Not a valid course code. Format: [code]/[number]/[code]*[number] (ex. cis or 1000 or cis*1000)")
+
         elif code.isnumeric() and len(code) != 4:
+
             print("Not a valid course code. Format: [code]/[number]/[code]*[number] (ex. cis or 1000 or cis*1000)")
+            
         else:
             inputFlag = False
 
     # Term (either S, F, or W)
     inputFlag = True
     while inputFlag:
-        term = input("Course season/term (hit enter to skip field): ").strip().upper()
+
+        term = input(
+            "Course season/term (hit enter to skip field): ").strip().upper()
+
         if term == 'S' or term == 'F' or term == 'W' or term == "":
             inputFlag = False
         else:
@@ -95,23 +108,32 @@ def cli(data):
     # Course Weight (must be a decimal value)
     inputFlag = True
     while inputFlag:
+
         weight = input("Course weight (hit enter to skip field): ").strip()
         if len(weight) != 0:
+
             try:
                 float(weight)
                 inputFlag = False
             except ValueError:
                 print("Valid course weight: 0.25, 0.5, 0.75, 1.0 etc")
                 continue
+
         else:
             inputFlag = False
 
-    # check for length of code name and if inputted for example cis*1300, cis1300, cis, 1300, or null 
+    # check for length of code name and if inputted for example cis*1300, cis1300, cis, 1300, or null
     # get course list depending on user input
     if code and len(code) > 4:
-        final_course_list = get_courses_with_code(data, code, weight, name, term)
+
+        final_course_list = get_courses_with_code(
+            data, code, weight, name, term)
+
     elif code and len(code) < 5:
-        final_course_list = get_courses_with_partial_code(data, code, weight, name, term)
+
+        final_course_list = get_courses_with_partial_code(
+            data, code, weight, name, term)
+
     else:
         final_course_list = get_courses_without_code(data, weight, name, term)
 
@@ -120,35 +142,66 @@ def cli(data):
         print("\nNo Courses Found.")
     else:
         print("\nCourses Found:")
+
+        #ask user whether or not to show course descriptions
+        print_desc = False
+
+        print_desc_query = input("Show Course Descriptions? [y/n] ")
+        if print_desc_query.lower() == "y" or print_desc_query.lower() == "yes":
+            print_desc = True
+
         for course in final_course_list:
-            print(course)
+
+            #put list of terms into a readable format
+            term_str = "( "
+            for term in course["terms"]:
+                term_str += term + " "
+            term_str += ")"
+
+            #print course info
+            print('\n', course['code'], course['name'],
+                  term_str, '[',course['weight'],']')
+            
+            #print description
+            if print_desc:
+                print('\t', course['description'])
+            print() #extra newline
 
     continueSearch = input("\nSearch again? [y/n] ")
     if continueSearch.lower() == "n" or continueSearch.lower() == "no":
         return False
     return True
-    
+
 
 # function to open JSON file
 def get_course_info(filename):
-    with open(filename, "r") as file:
-        data = file.read()
-        coursedata = json.loads(data)
+    try:
+        with open(filename, "r") as file:
+            data = file.read()
+            coursedata = json.loads(data)
+            
+    #if the file isnt found, return nothing
+    except FileNotFoundError as e:
+        return {}
     return coursedata
 
 
 def main():
-    try:
-        course_info = get_course_info("course_info.json")
-        print("\nWelcome to UoG Course Search")
 
-        while True:
-            keep_going = cli(course_info)
-            if not keep_going:
-                break
-    except:
-        print("File not found, run scraper.py")
-        
+    course_info = get_course_info("course_info.json")
+    
+    #if the get_course_info function failed, don't run
+    if not course_info:
+        print("\nFile not found, run scraper.py")
+        return
+
+    print("\nWelcome to UoG Course Search")
+
+    while True:
+        keep_going = cli(course_info)
+        if not keep_going:
+            break
+
 
 if __name__ == '__main__':
     main()
