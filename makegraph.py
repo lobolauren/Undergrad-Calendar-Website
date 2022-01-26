8a8881b8b9c5a5df090eb6fc1db0d7058a3a33a3
Malcolm Wright <mwrigh25@uoguelph.ca>
2022-01-26 13:41:59 -0500

    Added helper functions for Graphviz and began unit testing helpers

--- makegraph.py
+++ makegraph.py
@@ -1,4 +1,5 @@
 import json
+import graphviz
 
 def makegraph(course_data):
     name = input("Course name or department: ").strip().lower()
@@ -11,3 +12,68 @@ def makegraph(course_data):
         if continueSearch.lower() == "n" or continueSearch.lower() == "no":
             return False
         return True
+
+# Makegraph helper functions
+def saveGraphToPDF(graph):
+    graph.format = 'pdf'
+    graph.render(directory="doctest-output").replace('\\', '/')
+    # opens graph in PDF viewer
+    graph.view()
+
+# Since we are dealing with pre-requsites, we want a directed graph (one-way)
+# So, we'll use a Digraph
+def createCourseGraph(filename):
+    graph = graphviz.Digraph(filename)
+    makeLegend(graph)
+    return graph
+
+# Make the Legend for the grapgh
+def makeLegend(graph):
+    with graph.subgraph(name='clusterLegend') as c:
+        c.attr(color = "black")
+        c.attr(label="Legend")
+        addOutsideDepartmentCourse(c, "Course Outside of Department")
+        addRegularCourse(c, "Normal Prerequisite Course")
+        c.edge("Course Outside of Department", "Normal Prerequisite Course")
+        showRequiredPrerequisiteCourse(c, "Mandatory Prerequisite Course", "Course")
+        showOptionalPrerequisiteCourse1(c, "One of these Prerequisite Course", "Course")
+        showOptionalPrerequisiteCourse2(c, "One of these Prerequisite Course", "Course")
+        showOptionalPrerequisiteCourse3(c, "One of these Prerequisite Course", "Course")
+        showOptionalPrerequisiteCourse4(c, "One of these Prerequisite Course", "Course")
+
+# Change colour of node when course is outside requested department
+def addOutsideDepartmentCourse(graph, outsideCourse):
+    graph.node(outsideCourse, color="red")
+
+def addRegularCourse(graph, course):
+    graph.node(course)
+
+def showRequiredPrerequisiteCourse(graph, requiredCourse, childCourse):
+    graph.edge(requiredCourse, childCourse, color="green")
+
+# For pre-requsities where you just need either one
+def showOptionalPrerequisiteCourse1(graph, requiredCourse, childCourse):
+    graph.edge(requiredCourse, childCourse, color="blue")
+def showOptionalPrerequisiteCourse2(graph, requiredCourse, childCourse):
+    graph.edge(requiredCourse, childCourse, color="yellow")
+def showOptionalPrerequisiteCourse3(graph, requiredCourse, childCourse):
+    graph.edge(requiredCourse, childCourse, color="red")
+def showOptionalPrerequisiteCourse4(graph, requiredCourse, childCourse):
+    graph.edge(requiredCourse, childCourse, color="purple")
+
+# Test code
+courseGraph = createCourseGraph("CourseGraph")
+# EX. 1300
+showRequiredPrerequisiteCourse(courseGraph, "CIS*1300", "CIS*2500")
+# EX. 1300
+showRequiredPrerequisiteCourse(courseGraph, "CIS*1300", "CIS*2170")
+# EX. (2500 or 2170), (1 of 1900, 2110, 2750)
+showOptionalPrerequisiteCourse1(courseGraph, "CIS*2500", "CIS*3760")
+showOptionalPrerequisiteCourse1(courseGraph, "CIS*2170", "CIS*3760")
+showOptionalPrerequisiteCourse2(courseGraph, "CIS*1900", "CIS*3760")
+showOptionalPrerequisiteCourse2(courseGraph, "CIS*2110", "CIS*3760")
+showOptionalPrerequisiteCourse2(courseGraph, "CIS*2750", "CIS*3760")
+# EX. 3760
+showRequiredPrerequisiteCourse(courseGraph, "CIS*3760", "CIS*4250")
+
+saveGraphToPDF(courseGraph)
