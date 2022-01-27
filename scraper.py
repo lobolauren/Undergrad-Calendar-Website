6c3acd294b1131785853d174df0884b1d4cf7270
Nigel Davis <ndavis10@uoguelph.ca>
2022-01-27 18:54:05 -0500

    improved parsing prerequistes

--- scraper.py
+++ scraper.py
@@ -1,4 +1,6 @@
 import json
+import time
+
 from posixpath import split
 from typing import List, final
 from playwright.sync_api import sync_playwright
@@ -33,6 +35,64 @@ def get_course_codes():
     return codes_list
 
 
+def remove_commas_between_brackets(s):
+    new_str = ''
+    inside_brackets = False
+    outer_bracket = ''
+    for c in s:
+        if c == ')' and outer_bracket == '(':
+            inside_brackets = False
+        if c == ']' and outer_bracket == '[':
+            inside_brackets = False
+        if not inside_brackets or (inside_brackets and c != ','):
+            new_str += c
+        if not inside_brackets and c == '(' or c == '[':
+            outer_bracket = c
+            inside_brackets = True
+    return new_str
+
+
+def get_prereqs(prereqs_el, prereqs_list):
+
+    prereqs = {
+        'reg_prereqs': [],
+        'eq_prereqs': []
+    }
+
+    # search list of prereqs to check if in a '1 of' block
+    if prereqs_el:
+        prereq_text = prereqs_el.inner_text()
+        prereq_text = remove_commas_between_brackets(prereq_text)
+        tokens = prereq_text.split(',')
+
+        ftr = lambda s: True if '(' in s or '[' in s else False
+        filtered_tokens = list(filter(ftr, tokens))
+
+        if not filtered_tokens and ('or' in prereq_text or 'of' in prereq_text):
+            prereqs['eq_prereqs'] = [prereqs_list]
+
+        else: # for each 'block' of prerequisits, check if any codes from the full list are contained in it, if so, add to the eq_prereqs list
+            prereqs_list_copy = prereqs_list[:]
+            for t in filtered_tokens:
+                eq_prereqs = []
+                for course in prereqs_list:
+                    if 'excluding' in t and course in t[t.index('excluding'):]:
+                        prereqs_list_copy.remove(course)
+                    elif course in t:
+                        eq_prereqs.append(course)
+                        if course in prereqs_list_copy:
+                            prereqs_list_copy.remove(course)
+
+                if len(eq_prereqs) > 1:
+                    prereqs['eq_prereqs'].append(eq_prereqs)
+                else:
+                    prereqs_list_copy.extend(eq_prereqs)
+
+            prereqs['reg_prereqs'] = prereqs_list_copy
+    
+    return prereqs
+
+
 # get individual course details from course object
 def get_course_details(course):
 
@@ -44,80 +104,7 @@ def get_course_details(course):
 
     course_offerings_el = course.query_selector('.detail-typically_offered')
     course_offerings = course_offerings_el.inner_text() if course_offerings_el else ''
-
-    course_prereqs_el = course.query_selector('.detail-prerequisite_s_ span') 
-    course_prereqs_a = course_prereqs_el.query_selector_all('a') if course_prereqs_el else []
-
-    final_prereqs = [req.inner_text() for req in course_prereqs_a]
-
-    prereqs = {
-        'reg_prereqs': [],
-        'eq_prereqs': []
-    }
-
-    #search list of prereqs to check if in a '1 of' block
-    if course_prereqs_el:
-        prereq_text = course_prereqs_el.inner_text()
-
-        #remove commas inside brackets
-        inside_brackets = False
-        new_str = ''
-        for c in prereq_text:
-            if c == ')':
-                inside_brackets = False
-            if not inside_brackets or (inside_brackets and c != ','):
-                new_str += c
-            if c == '(':
-                inside_brackets = True
-
-        prereq_text = new_str
-
-        sub_prereq_text = prereq_text.split(',')
-
-        ftr = lambda s: True if '(' in s or '[' in s else False
-
-        eq_prereqs = list(filter(ftr,sub_prereq_text))
-
-        if not eq_prereqs and ('or' in prereq_text or 'of' in prereq_text):
-            prereqs['eq_prereqs'] = [final_prereqs]
-
-        else:
-
-            final_prereqs_copy = final_prereqs[:]
-
-            #for each 'block' of prerequisits, check if any codes from the full list are contained in it, if so, add to the eq_prereqs list
-            for sub in eq_prereqs:
-                sub_list = []
-
-                for code in final_prereqs:
-
-                    if code in sub:
-                        sub_list.append(code)
-                        #double checking
-                        if code in final_prereqs_copy:
-                            final_prereqs_copy.remove(code)
-
-                prereqs['eq_prereqs'].append(sub_list)
-            
-            prereqs['reg_prereqs'] = final_prereqs_copy
-
-        #consistency for courses that no longer exist (and therefore dont appear in the full list)
-        #ex. ([existing*course] or [doesnt*exist]), put existing course in reg_prereqs list
-
-        eq_prereqs_copy = prereqs['eq_prereqs'][:]
-
-        idx = 0
-        for block in eq_prereqs_copy:
-
-            if len(block) < 2:
-
-                if block:
-                    prereqs['reg_prereqs'].append(block[0])
-                prereqs['eq_prereqs'].pop(idx)
-                idx -= 1
-            idx += 1
-
-
+    
     course_terms = []
     if 'fall' in course_offerings.lower():
         course_terms.append('F')
@@ -126,6 +113,12 @@ def get_course_details(course):
     if 'summer' in course_offerings.lower():
         course_terms.append('S')
 
+    course_prereqs_el = course.query_selector('.detail-prerequisite_s_ span') 
+    course_prereqs_a = course_prereqs_el.query_selector_all('a') if course_prereqs_el else []
+
+    prereqs_a_text = [req.inner_text() for req in course_prereqs_a]
+    prereqs = get_prereqs(course_prereqs_el, prereqs_a_text)
+    
     course_weight_el = course.query_selector('.detail-hours_html')
     course_weight = float(course_weight_el.inner_text()[1:-1])
 
@@ -154,12 +147,14 @@ def get_course_info(course_codes: List[str]):
 
         for code in course_codes:
             course_info[code] = []
-            page.goto(f'https://calendar.uoguelph.ca/undergraduate-calendar/course-descriptions/{code}/')
+            if code == 'iaef':
+                page.goto(f'https://calendar.uoguelph.ca/undergraduate-calendar/course-descriptions/ieaf/')
+            else:
+                page.goto(f'https://calendar.uoguelph.ca/undergraduate-calendar/course-descriptions/{code}/')
 
             courses = page.query_selector_all('.courseblock')
             for course in courses:
                 course_info[code].append(get_course_details(course))
-            
 
         browser.close()
 
@@ -173,13 +168,22 @@ def save_dict_as_json(dict, filename):
 
 
 def main():
+
+    debug = True
+    start = time.time()
+
     print("Scraping...")
     codes = get_course_codes()
+
     print("Getting course info... (this may take a minute)")
     course_info = get_course_info(codes)
+
     print("Saving file...")
     save_dict_as_json(course_info, filename='course_info.json')
-    print("Done.")
+
+    end = time.time()
+    if debug:
+        print(f"Done in {int(end - start)}s.")
 
 
 if __name__ == '__main__':
