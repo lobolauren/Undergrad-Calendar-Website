ed73e56b43fbca75d0e0a04a9dc15796ec9873be
Malcolm Wright <mwrigh25@uoguelph.ca>
2022-01-28 09:16:44 -0500

    Added comments, fixed issue with test, and renamed graph directory

--- tests/test_makegraph.py
+++ tests/test_makegraph.py
@@ -7,35 +7,36 @@ sys.path.insert(0, "../")
 import makegraph
 
 class TestMakeGraph(unittest.TestCase):
-    def test_graphCreation(self):
+    def test_graph_creation(self):
+        # create graph and check for filename
         fileName = "MyGraph"
         myGraph = makegraph.createCourseGraph(fileName)
         self.assertIsNotNone(myGraph)
         self.assertEqual(myGraph.filename, fileName + ".gv")
-
         
-    def test_saveGraphToPDF(self):
+    def test_save_graph_to_PDF(self):
         fileName = "MyGraph"
         try:
+            # Create graph with test edge and see if PDF file is created
             myGraph = makegraph.createCourseGraph(fileName)
             myGraph.edge("nodeTest1", "nodeTest2")
             makegraph.saveGraphToPDF(myGraph)
-            sys.path.insert(0, "./doctest-output/")
-            self.assertTrue(os.path.isdir("./doctest-output/"))
-            self.assertTrue(os.path.isfile("./doctest-output/" + fileName + ".gv"))
-            self.assertTrue(os.path.isfile("./doctest-output/" + fileName + ".gv.pdf"))
+            sys.path.insert(0, "./graph-output/")
+            self.assertTrue(os.path.isdir("./graph-output/"))
+            self.assertTrue(os.path.isfile("./graph-output/" + fileName + ".gv"))
+            self.assertTrue(os.path.isfile("./graph-output/" + fileName + ".gv.pdf"))
 
             # make sure graph file isn't empty (.gv)
-            sys.path.insert(0, "./doctest-output/")
-            with open("./doctest-output/" + fileName + ".gv", 'r') as testFile:
+            sys.path.insert(0, "./graph-output/")
+            with open("./graph-output/" + fileName + ".gv", 'r') as testFile:
                 contents = testFile.read()
                 self.assertIsNotNone(contents)
                 self.assertTrue("nodeTest1 -> nodeTest2" in contents)
 
         finally:
             # delete files after
-            os.remove("./doctest-output/" + fileName + ".gv.pdf") 
-            os.remove("./doctest-output/" + fileName + ".gv")
+            os.remove("./graph-output/" + fileName + ".gv.pdf") 
+            os.remove("./graph-output/" + fileName + ".gv")
 
 if __name__ == '__main__':
     unittest.main() # allows you to run by doing python3 test_makegraph.py
--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -9,13 +9,14 @@ import scraper
 
 class TestScraper(unittest.TestCase):
 
-    def test_getCourseCodeFunction(self):
+    def test_get_course_code_function(self):
+        # gets list of all course codes and compares with expected amount
         courseCodes = scraper.get_course_codes()
         self.assertIsNotNone(courseCodes)
         self.assertEqual(len(courseCodes), test_constants.NUM_COURSE_CODES)
         self.assertEqual(courseCodes[0], test_constants.TEST_COURSE_CODE)
 
-    def test_getCourseInfo(self):
+    def test_get_course_info(self):
         # test scrapper with one course only
         course_info = scraper.get_course_info(test_constants.TEST_COURSE)
         self.assertIsNotNone(course_info)
@@ -24,6 +25,7 @@ class TestScraper(unittest.TestCase):
         accountingCourses = courses[test_constants.TEST_COURSE_CODE]
         self.assertIsNotNone(courses)
 
+        # first accounting course must be AACT*1220
         introAccounting = accountingCourses[0]
         self.assertIsNotNone(introAccounting)
         self.assertEqual(introAccounting["code"], "ACCT*1220")
@@ -31,47 +33,54 @@ class TestScraper(unittest.TestCase):
         self.assertEqual(introAccounting["terms"], ["F", "W"])
         self.assertEqual(introAccounting["weight"], 0.5)
 
-    def test_saveDictAsJSON(self):
+    def test_save_dict_as_JSON(self):
+        # save a test dictionary object using the functcion
         try:
             scraper.save_dict_as_json(test_constants.TEST_DICT, test_constants.TEST_FILENAME)
             with open(test_constants.TEST_FILENAME, 'r') as testFile:
                 contents = testFile.read()
         finally:
             os.remove(test_constants.TEST_FILENAME) # delete JSON once done
+
+        # convert to dictionary and compare with original dictionary
         self.assertIsNotNone(contents)
-        # convert to dictionary
         jsonObject = json.loads(contents)
         self.assertEqual(jsonObject, test_constants.TEST_DICT)
 
     def test_requisites(self):
+        # get test course info
         course_info = scraper.get_course_info(test_constants.TEST_COURSE)
         self.assertIsNotNone(course_info)
         courses = course_info[test_constants.COURSES_CONSTANT]
         accountingCourses = courses[test_constants.TEST_COURSE_CODE]
 
         #Check reg_prereqs and eq_prereqs empty
-        self.assertEqual(accountingCourses[0]["prereqs"]["reg_prereqs"], [])
-        self.assertEqual(accountingCourses[0]["prereqs"]["eq_prereqs"], [])
+        acct1220Index = 0
+        acct1240Index = 1
+        self.assertEqual(accountingCourses[acct1220Index]["code"], "ACCT*1220")
+        self.assertEqual(accountingCourses[acct1220Index]["prereqs"]["reg_prereqs"], [])
+        self.assertEqual(accountingCourses[acct1220Index]["prereqs"]["eq_prereqs"], [])
 
         # Check reg_prereqs
-        self.assertEqual(accountingCourses[1]["prereqs"]["reg_prereqs"][0], "ACCT*1220")
-        self.assertEqual(accountingCourses[1]["prereqs"]["eq_prereqs"], [])
+        self.assertEqual(accountingCourses[acct1240Index]["code"], "ACCT*1240")
+        self.assertEqual(accountingCourses[acct1240Index]["prereqs"]["reg_prereqs"], [])
+        self.assertEqual(accountingCourses[acct1240Index]["prereqs"]["eq_prereqs"][0], ["ACCT*1220"])
 
-        # Check eq_prereqs
+        # Check eq_prereqs lists
         courses = course_info[test_constants.COURSES_CONSTANT]
         agrCourses = courses[test_constants.TEST_COURSE_CODE_AGR]
 
-        self.assertEqual(agrCourses[1]["prereqs"]["reg_prereqs"], [])
-        self.assertEqual(agrCourses[1]["prereqs"]["eq_prereqs"][0][0], "AGR*1110")
-        self.assertEqual(agrCourses[1]["prereqs"]["eq_prereqs"][0][1], "AGR*2150")
-        self.assertEqual(agrCourses[1]["prereqs"]["eq_prereqs"][1][0], "BIOL*1050")
-        self.assertEqual(agrCourses[1]["prereqs"]["eq_prereqs"][1][1], "BIOL*1070")
-
-        
-        
-
-
-        
+        agr2050Index = 1
+        firstEqList = ["AGR*1110","AGR*2150"]
+        secondEqList = ["BIOL*1050","BIOL*1070"]
+        self.assertEqual(agrCourses[agr2050Index]["code"], "AGR*2050")
+        self.assertEqual(agrCourses[agr2050Index]["prereqs"]["reg_prereqs"], [])
+        self.assertEqual(agrCourses[agr2050Index]["prereqs"]["eq_prereqs"][0][0], "AGR*1110")
+        self.assertEqual(agrCourses[agr2050Index]["prereqs"]["eq_prereqs"][0][1], "AGR*2150")
+        self.assertEqual(agrCourses[agr2050Index]["prereqs"]["eq_prereqs"][0], firstEqList)
+        self.assertEqual(agrCourses[agr2050Index]["prereqs"]["eq_prereqs"][1][0], "BIOL*1050")
+        self.assertEqual(agrCourses[agr2050Index]["prereqs"]["eq_prereqs"][1][1], "BIOL*1070")
+        self.assertEqual(agrCourses[agr2050Index]["prereqs"]["eq_prereqs"][1], secondEqList)
 
 if __name__ == '__main__':
-    unittest.main() # allows you to run by doing python3 testScraper.py
+    unittest.main() # allows you to run by doing python3 test_scraper.py
