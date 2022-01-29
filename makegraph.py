ce169c7efe64c599393b4c2fc88b0b99e1212a04
holmesg <holmesg@uoguelph.ca>
2022-01-28 19:49:06 -0500

    some error checking for makegraph + choose your own file name

--- makegraph.py
+++ makegraph.py
@@ -6,9 +6,9 @@ import graphviz
 COLORS = ['blue', 'orange', 'red', 'purple', 'yellow']
 
 # Makegraph helper functions
-def saveGraphToPDF(graph):
+def saveGraphToPDF(graph, file_name):
     graph.format = 'pdf'
-    graph.render(directory="graph-output").replace('\\', '/')
+    graph.render(filename=file_name, directory="graph-output").replace('\\', '/')
     # opens graph in PDF viewer
     # graph.view()
 
@@ -66,9 +66,9 @@ def add_eq_prereqs(graph, eq_prereqs, course):
 
 def parsingPrereq(graph, code, data):
 
+    print("checking prerequisites...")
     course_attr = code[:code.index('*')].lower()
     prereq_list = []
-    print(course_attr)
 
     for course_value in data["courses"][course_attr]:
         if course_value["code"] == code.upper():
@@ -91,6 +91,8 @@ def parsingPrereq(graph, code, data):
 
 
 def parsingDepartment(graph, code,data):
+
+    print("checking department...")
     for course_value in data["courses"][code]:
         addRegularCourse(graph,course_value["code"])
         
@@ -108,22 +110,46 @@ def parsingDepartment(graph, code,data):
 
 def makegraph(course_data):
 
-    name = input("Course name or department: ").strip().lower()
+    name = input("Course name or department [q to quit]: ").strip().lower()
+    
+    # allow user to quit
+    if name.lower() == 'q' or name.lower() == 'quit':
+        return False
+
+    # make sure name is in the data
+    try:
+        course_attr = name
+        # only get this if the input is a full course name
+        if '*' in name:
+            course_attr = name[:name.index('*')]
+
+        course_data['courses'][course_attr]
+    except KeyError:
+        print(name + ' not found...')
+        return True
 
     courseGraph = createCourseGraph("graph1")
+    print("making graph...")
+
 
     if len(name) > 4:
         parsingPrereq(courseGraph, name, course_data)
     else:
         parsingDepartment(courseGraph, name,course_data)
-        
-    saveGraphToPDF(courseGraph)
+
+    # file naming
+    # remove special characters
+    file_name = name.replace('*','')+"-graph"
+    file_name_query = input("Name graph file? [y/n]: ")
+    
+    if file_name_query.lower() == "y" or file_name_query.lower() == "yes":
+        file_name = input("Enter graph name: ")
+
+    saveGraphToPDF(courseGraph, file_name)
 
     # Course code (ex. CIS*1300 or CIS or 1300)
     inputFlag = True
     while inputFlag:
-
-        print("making graph...")
 
         continueSearch = input("\n Graph another course? [y/n] ")
 
