c5a74c2b550878057b76cb169cf59735180515cb
Garrett Holmes <holmesg@uoguelph.ca>
2022-02-03 11:04:35 -0500

    added some more stuff to install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
 # Installs all required python libraries
 # Run with: 'sh install.sh'
 
-pip install -U -r requirements.txt
+#install python
+if [ -x "$(command -v python)" ]; then
+    echo "python already installed"
+else
+    sudo apt-get install python
+fi
+
+#install python3 just in case
+if [ -x "$(command -v python3)" ]; then
+    echo "python3 already installed"
+else
+    sudo apt-get install python3
+fi
 
 #install chromium
 if [ -x "$(command -v chromium)" ]; then
@@ -12,7 +24,12 @@ fi
 
 #install Graphviz
 if [ -x "$(command -v dot)" ]; then
-    echo "graphciz already installed"
+    echo "graphviz already installed"
 else
-    sudo apt install graphviz 
+    sudo apt install graphviz
 fi
+
+pip install -U -r requirements.txt
+pip3 install -U -r requirements.txt
+
+playwright install
