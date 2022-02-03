820b2799b5309a08e387c38bbd154d94ab673b24
Nigel Davis <ndavis10@uoguelph.ca>
2022-02-03 12:45:07 -0500

    added better documentation

--- scraper.py
+++ scraper.py
@@ -10,9 +10,8 @@ def get_course_codes():
 
     codes_list = []
 
-    # Using synchronus playwright
     with sync_playwright() as pw:
-        # launch chromium headless
+
         browser = pw.chromium.launch(headless=True)
         page = browser.new_page()
 
@@ -20,20 +19,20 @@ def get_course_codes():
 
         # select part of page containing links to course codes
         codes_region = page.query_selector('.az_sitemap')
-        codes = codes_region.query_selector_all('li')[27:] #select all lists (ignore first 27, which are letters of the alphabet)
-        # for each code in the list of names
+        codes = codes_region.query_selector_all('li')[27:] # ignore first 27 because they are just letters of the alphabet for navigation
+
         for code in codes: 
             code_str = code.inner_text()
-            # extract the code from the text
-            code_str_final = code_str[code_str.index('(')+1:-1] # finds index of first bracket, grabs string between that index & the last character
-            # add code to the list in lower case (for navigation on the website)
+            # finds index of first bracket, grabs string between that index and the last character
+            code_str_final = code_str[code_str.index('(')+1:-1] 
             codes_list.append(code_str_final.lower())
 
         browser.close()
 
     return codes_list
 
-
+# remove all commas from between brackets in a string
+# ex: "(,,),(,),,(,,,),[,(,,)](,,),,," -> "(),(),,(),[()](),,,"
 def remove_commas_between_brackets(s):
     new_str = ''
     inside_brackets = False
@@ -55,60 +54,87 @@ def get_prereqs(prereqs_el, prereqs_list, course_code):
 
     prereqs = {
         'reg_prereqs': [],
-        'eq_prereqs': []
+        'eq_prereqs': [] # list of lists each sublist represents related prerequisites
     }
 
-    # search list of prereqs to check if in a '1 of' block
     if prereqs_el:
         prereq_text = prereqs_el.inner_text()
+
+        # the reason we do this is because we noticed that every block of related prerequisites is contained within 
+        # brackets that are seperated by commas. We then remove the commas from between the brackets so we can seperate
+        # each block of related prerequisites by splitting on the comma character
         prereq_text = remove_commas_between_brackets(prereq_text)
         tokens = prereq_text.split(',')
 
+        # we now only want to look at courses that are in a block of related prerequisites so we get all the blocks that
+        # contain the bracket characters
         ftr = lambda s: True if '(' in s or '[' in s else False
         filtered_tokens = list(filter(ftr, tokens))
 
+        # there there aren't any blocks that in the filtered list then check if the prerequisite text contains the words
+        # 'or' or 'of' and if there is add them
         if not filtered_tokens and ('or' in prereq_text or 'of' in prereq_text):
+            # if there is only one prerequisite add it to regular prerequisites otherwise add them as related prerequisites
             if len(prereqs_list) < 2:
                 prereqs['reg_prereqs'].extend(prereqs_list)
             else:
                 prereqs['eq_prereqs'] = [prereqs_list]
 
-        else: # for each 'block' of prerequisits, check if any codes from the full list are contained in it, if so, add to the eq_prereqs list
+        else: 
+            # copy list because we are going to be removing elements from it while looping through it
             prereqs_list_copy = prereqs_list[:]
+
+            # for each block of related prerequisites, go through and remove any course from the that exists in the block 
+            # from the original prerequisites list
             for t in filtered_tokens:
+                
                 eq_prereqs = []
                 for course in prereqs_list:
+                    # if the course itself exists in its prerequisties string, remove it and do nothing
                     if course == course_code:
                         prereqs_list_copy.remove(course)
+                    # if a course is in the string but appears after the word 'excluding', remove it and do nothing
                     elif 'excluding' in t and course in t[t.index('excluding'):]:
                         prereqs_list_copy.remove(course)
+                    # if it finds a course remove it and add it to the temporary list of related prerequisites
                     elif course in t:
                         eq_prereqs.append(course)
                         if course in prereqs_list_copy:
                             prereqs_list_copy.remove(course)
 
+                # if the block only had one prerequisite, make it a regular prerequisite, otherwise add the related prerequisites
                 if len(eq_prereqs) > 1:
                     prereqs['eq_prereqs'].append(eq_prereqs)
                 else:
                     prereqs_list_copy.extend(eq_prereqs)
 
+            # at this point any prerequisites left in the list are not related and must be required
             prereqs['reg_prereqs'] = prereqs_list_copy
     
     return prereqs
 
 
+# finds an element and returns its inner text
+def get_element_text(parent_el, query):
+    child_el = parent_el.query_selector(query)
+    return child_el.inner_text() if child_el else ''
+
+
 # get individual course details from course object
 def get_course_details(course):
 
-    course_code_el = course.query_selector('.detail-code')
-    course_code = course_code_el.inner_text()
+    course_code = get_element_text(course, '.detail-code')
+    course_title = get_element_text(course, '.detail-title')
+    course_availability = get_element_text(course, '.detail-typically_offered')
+    course_desc = get_element_text(course, '.courseblockextra')
+    course_restrictions = get_element_text(course, '.detail-restriction_s_ span')
+    course_department = get_element_text(course, '.detail-department_s_ span')
+    course_location = get_element_text(course, '.detail-location_s_ span')
+    course_corequisites = get_element_text(course, '.detail-co_requisite_s_ span')
+    course_offering = get_element_text(course, '.detail-offering span')
+    de_offering = True if 'distance education' in course_offering.lower() else False
+    course_weight = float(get_element_text(course, '.detail-hours_html')[1:-1])
 
-    course_title_el = course.query_selector('.detail-title')
-    course_title = course_title_el.inner_text()
-
-    course_availability_el = course.query_selector('.detail-typically_offered')
-    course_availability = course_availability_el.inner_text() if course_availability_el else ''
-    
     course_terms = []
     if 'fall' in course_availability.lower():
         course_terms.append('F')
@@ -117,37 +143,11 @@ def get_course_details(course):
     if 'summer' in course_availability.lower():
         course_terms.append('S')
 
-    course_prereqs_el = course.query_selector('.detail-prerequisite_s_ span') 
+    course_prereqs_el = course.query_selector('.detail-prerequisite_s_ span')
+    # get all prerequisite links and add their text to a list
     course_prereqs_a = course_prereqs_el.query_selector_all('a') if course_prereqs_el else []
-
     prereqs_a_text = [req.inner_text() for req in course_prereqs_a]
     prereqs = get_prereqs(course_prereqs_el, prereqs_a_text, course_code)
-    
-    course_weight_el = course.query_selector('.detail-hours_html')
-    course_weight = float(course_weight_el.inner_text()[1:-1])
-
-    course_desc_el = course.query_selector('.courseblockextra')
-    course_desc = course_desc_el.inner_text() if course_desc_el else ''
-
-    course_offering_el = course.query_selector('.detail-offering span')
-    course_offering = course_offering_el.inner_text() if course_offering_el else ''
-
-    if 'Distance Education' in course_offering:
-        de_offering = True
-    else:
-        de_offering = False
-
-    course_restrictions_el = course.query_selector('.detail-restriction_s_ span')
-    course_restrictions = course_restrictions_el.inner_text() if course_restrictions_el else ''
-
-    course_department_el = course.query_selector('.detail-department_s_ span')
-    course_department = course_department_el.inner_text() if course_department_el else ''
-
-    course_location_el = course.query_selector('.detail-location_s_ span')
-    course_location = course_location_el.inner_text() if course_location_el else ''
-
-    course_corequisites_el = course.query_selector('.detail-co_requisite_s_ span')
-    course_corequisites = course_corequisites_el.inner_text() if course_corequisites_el else ''
 
     return {
         'code': course_code.upper(),
@@ -156,7 +156,7 @@ def get_course_details(course):
         'weight': course_weight,
         'description': course_desc,
         'prereqs': prereqs,
-        'offerings': de_offering,
+        'de': de_offering,
         'restrictions': course_restrictions,
         'department': course_department,
         'location': course_location,
@@ -175,8 +175,9 @@ def get_course_info(course_codes: List[str]):
         page = browser.new_page()
 
         for code in course_codes:
+
             course_info[code] = []
-            if code == 'iaef':
+            if code == 'iaef': # edge case since the url for the course descriptions is wrong
                 page.goto(f'https://calendar.uoguelph.ca/undergraduate-calendar/course-descriptions/ieaf/')
             else:
                 page.goto(f'https://calendar.uoguelph.ca/undergraduate-calendar/course-descriptions/{code}/')
@@ -205,6 +206,7 @@ def get_degree_program_links(page: Page):
     for list_el in list_els:
         relative_link = list_el.get_attribute("href")
         links.append(f'https://calendar.uoguelph.ca{relative_link}')
+
     return links
 
 
@@ -230,6 +232,7 @@ def get_major_requirements(page: Page, links): # Specific major program
     programs_reqirements = {}
 
     for degree_programs_link in links:
+
         degree_programs_link += '#requirementstext'
         page.goto(degree_programs_link)
         
@@ -275,6 +278,7 @@ def get_major_requirements(page: Page, links): # Specific major program
     return programs_reqirements
 
 def get_program_info():
+
     with sync_playwright() as pw: 
 
         browser = pw.chromium.launch(headless=True)
@@ -286,7 +290,6 @@ def get_program_info():
 
         print('Getting program requirements')
         program_info = get_major_requirements(page, program_links)
-
 
         browser.close()
 
