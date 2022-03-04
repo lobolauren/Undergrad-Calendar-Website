27c03c24aba9fce81f2255d6eba0ce006e012522
Nigel Davis <ndavis10@uoguelph.ca>
2022-03-04 10:46:58 -0500

    added basic navigation bar

---
+++ course-utility/src/components/global/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Navbar, Container, Nav} from 'react-bootstrap'
+
+const Header = () => {
+    return (
+        <Navbar bg="dark" expand="lg" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Guelph Course Utility</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Course Search</Nav.Link>
+                        <Nav.Link href="#link">Make Graph</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
