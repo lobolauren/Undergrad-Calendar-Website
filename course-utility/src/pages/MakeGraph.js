c6a78bbc7001f57a3b1dd25cfd8b1101efa76fe4
Samantha Preisig <spreisig@uoguelph.ca>
2022-03-06 15:23:38 -0500

    Update MakeGraph.js

--- course-utility/src/pages/MakeGraph.js
+++ course-utility/src/pages/MakeGraph.js
@@ -4,19 +4,18 @@ import InfoModal from '../components/makegraph/InfoModal'
 import MakegraphOption from '../components/makegraph/makegraphOption'
 import { Form, Button, Row, Col } from 'react-bootstrap'
 
-const MakeGraph = () => {
+const MakeGraph=()=>{
     
   return (
     <Container className="mt-40">
       <span>
-        <h1>Make Graph <InfoModal /></h1>
+        <h1>Make Graph <InfoModal/></h1>
         <br/><br/><br/>
         <h2>Desired Graph Style: </h2>
        
-        <MakegraphOption/>
       </span>
+      <MakegraphOption/>
     </Container>
-    
   )
 }
 
