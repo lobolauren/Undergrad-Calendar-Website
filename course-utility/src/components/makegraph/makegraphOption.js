13abc121e7280ff3eb2a23ca98d0bddf5a2abe36
Lauren Lobo <lobol@uoguelph.ca>
2022-03-06 14:35:24 -0500

    Add new file

---
+++ course-utility/src/components/makegraph/makegraphOption.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+
+const MakegraphOption=()=>{
+    return (
+        
+            <Form bg="dark" expand="lg" variant="dark">
+                
+                <h1>testing hi hello</h1>
+                    
+                    
+                <Button type='submit'>Search</Button>
+            </Form>
+    )
+}
+   
+
+export default MakegraphOption
