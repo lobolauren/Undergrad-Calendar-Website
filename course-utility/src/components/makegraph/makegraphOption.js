9054d74638dd0238ce32db8941122845bca5f54d
Samantha Preisig <spreisig@uoguelph.ca>
2022-03-06 15:21:36 -0500

    Update makegraphOption.js

--- course-utility/src/components/makegraph/makegraphOption.js
+++ course-utility/src/components/makegraph/makegraphOption.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 import { Form, Button, Row, Col } from 'react-bootstrap'
 
 const MakegraphOption=()=>{
@@ -8,10 +8,46 @@ const MakegraphOption=()=>{
         
             <Form bg="dark" expand="lg" variant="dark">
                 
-                <h1>testing hi hello</h1>
+                <Row>
+                    <div className='termBox'>
+                    <link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap.min.css" />
+                    <link rel="stylesheet" type="text/css" href="https://netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" />
+                    <link rel="stylesheet" type="text/css" href="https://flatlogic.github.io/awesome-bootstrap-checkbox/demo/build.css" />
+                        <div class="checkbox  checkbox-circle">
+                            <input id="chkDefault" type="checkbox"/>
+                            <label for="chkDefault">
+                                Department Graph
+                            </label>
+                        </div>
+                        <div class="checkbox  checkbox-circle">
+                            <input id="chkDefault" type="checkbox"/>
+                            <label for="chkDefault">
+                                Program Graph
+                            </label>
+                        </div>
+                        <div class="checkbox  checkbox-circle">
+                            <input id="chkDefault" type="checkbox"/>
+                            <label for="chkDefault">
+                                Course Graph
+                            </label>
+                        </div>
+                        <div class="checkbox  checkbox-circle">
+                            <input id="chkDefault" type="checkbox"/>
+                            <label for="chkDefault">
+                                Catalogue Graph
+                            </label>
+                        </div>
+                    </div>
+                </Row>
+                <Row>
+                    <Form.Group as={Col}>
+                        <Form.Label>Desired filename (optional):</Form.Label>
+                        <Form.Control type='name' placeholder='Filename' />
+                    </Form.Group>
+                </Row>
                     
                     
-                <Button type='submit'>Search</Button>
+                <Button type='submit'>Makegraph</Button>
             </Form>
     )
 }
