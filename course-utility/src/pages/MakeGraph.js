69182a8362589d4048a5b7ca5ff15d622af53bab
Lauren Lobo <lobol@uoguelph.ca>
2022-03-06 14:34:32 -0500

    Update MakeGraph.js

--- course-utility/src/pages/MakeGraph.js
+++ course-utility/src/pages/MakeGraph.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import InfoModal from '../components/makegraph/InfoModal'
+import MakegraphOption from '../components/makegraph/MakegraphOption'
+import { Form, Button, Row, Col } from 'react-bootstrap'
 
 const MakeGraph = () => {
     
@@ -8,8 +10,13 @@ const MakeGraph = () => {
     <Container className="mt-40">
       <span>
         <h1>Make Graph <InfoModal /></h1>
+        <br/><br/><br/>
+        <h2>Desired Graph Style: </h2>
+       
+        <MakegraphOption/>
       </span>
     </Container>
+    
   )
 }
 
