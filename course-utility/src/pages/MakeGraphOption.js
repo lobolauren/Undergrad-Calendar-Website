94bb2aeae2b75545d097c025eb7cc85e97d1d133
Nigel Davis <ndavis10@uoguelph.ca>
2022-03-07 16:07:18 -0500

    initial flask setup

---
+++ course-utility/src/pages/MakeGraphOption.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { Form, Button } from 'react-bootstrap'
+
+const MakegraphOption = () => {
+    return (
+        <div className='makegraphOption'>
+            <Form bg="dark" expand="lg" variant="dark">
+    
+                <h1>testing hi hello</h1>
+    
+    
+                <Button type='submit'>Search</Button>
+            </Form>
+        </div>
+    )
+}
+
+
+export default MakegraphOption
