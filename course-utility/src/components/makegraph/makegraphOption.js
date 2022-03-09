6a9d638036ffb03d0d8850121c52eb7810acdbae
Malcolm Wright <wrightmbw01@gmail.com>
2022-03-09 12:07:51 -0500

    Added react-tooltip for the different graph styles for the makegraphOption component.

--- course-utility/src/components/makegraph/makegraphOption.js
+++ course-utility/src/components/makegraph/makegraphOption.js
@@ -1,7 +1,8 @@
 import React from 'react';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
-import { Form, Button, Row, Col } from 'react-bootstrap'
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import ReactTooltip from "react-tooltip";
 
 const MakegraphOption=()=>{
     return (
@@ -14,28 +15,44 @@ const MakegraphOption=()=>{
                     <link rel="stylesheet" type="text/css" href="https://netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" />
                     <link rel="stylesheet" type="text/css" href="https://flatlogic.github.io/awesome-bootstrap-checkbox/demo/build.css" />
                         <div class="checkbox  checkbox-circle">
-                            <input id="chkDefault" type="checkbox"/>
-                            <label for="chkDefault">
+                            <input id="chkDefault" type="checkbox" data-tip data-for="departmentTip"/>
+                            <label for="chkDefault" data-tip data-for="departmentTip">
                                 Department Graph
                             </label>
+
+                            <ReactTooltip id="departmentTip" place="right" effect="solid">
+                                Tooltip for the department graph style
+                            </ReactTooltip>
                         </div>
                         <div class="checkbox  checkbox-circle">
-                            <input id="chkDefault" type="checkbox"/>
-                            <label for="chkDefault">
+                            <input id="chkDefault" type="checkbox" data-tip data-for="programTip"/>
+                            <label for="chkDefault" data-tip data-for="programTip">
                                 Program Graph
                             </label>
+
+                            <ReactTooltip id="programTip" place="right" effect="solid">
+                                Tooltip for the program graph style
+                            </ReactTooltip>
                         </div>
                         <div class="checkbox  checkbox-circle">
-                            <input id="chkDefault" type="checkbox"/>
-                            <label for="chkDefault">
+                            <input id="chkDefault" type="checkbox" data-tip data-for="courseTip"/>
+                            <label for="chkDefault" data-tip data-for="courseTip">
                                 Course Graph
                             </label>
+
+                            <ReactTooltip id="courseTip" place="right" effect="solid">
+                                Tooltip for the course graph style
+                            </ReactTooltip>
                         </div>
                         <div class="checkbox  checkbox-circle">
-                            <input id="chkDefault" type="checkbox"/>
-                            <label for="chkDefault">
+                            <input id="chkDefault" type="checkbox" data-tip data-for="catalogueTip"/>
+                            <label for="chkDefault" data-tip data-for="catalogueTip">
                                 Catalogue Graph
                             </label>
+
+                            <ReactTooltip id="catalogueTip" place="right" effect="solid">
+                                Tooltip for the catalogue graph style
+                            </ReactTooltip>
                         </div>
                     </div>
                 </Row>
