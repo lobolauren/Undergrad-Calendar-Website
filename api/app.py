4814a719654e88bba99938d7d324042a6f83f0be
Malcolm Wright <wrightmbw01@gmail.com>
2022-03-09 11:55:22 -0500

    Added basic get request that returns json

--- api/app.py
+++ api/app.py
@@ -1,3 +1,5 @@
+import os
+import json
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,3 +7,21 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+# function to open JSON file
+def get_course_info(filename):
+    try:
+        with open(filename, 'r') as file:
+            data = file.read()
+            coursedata = json.loads(data)
+            
+    # if the file isnt found, return nothing
+    except FileNotFoundError as e:
+        return {}
+    return coursedata
+
+
+@app.route("/get_course_data")
+def get_course_data_json():
+    return get_course_info("course_info.json")
