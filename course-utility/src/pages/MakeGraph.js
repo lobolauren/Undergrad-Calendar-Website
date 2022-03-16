5820c966e9246b9dbbf7a73281b76b2060222cb1
Samantha Preisig <spreisig@uoguelph.ca>
2022-03-16 13:02:49 -0400

    Page navigation to graphs

--- course-utility/src/pages/MakeGraph.js
+++ course-utility/src/pages/MakeGraph.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 import { Container } from 'react-bootstrap'
 import InfoModal from '../components/makegraph/InfoModal'
 import MakeGraphForm from '../components/makegraph/MakeGraphForm'
 import { Form, Button, Row, Col } from 'react-bootstrap'
 
 const MakeGraph=()=>{
+
+  const navigate = useNavigate();
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    let graphType = event.target.graphType.value;
+    let courseCode = event.target.courseCode.value;
+
+    let graphWanted = {
+        "type": graphType,
+        "code": courseCode
+    }
+
+    navigate('/graph/' + graphWanted["type"] + '/' + graphWanted["code"]);
+  }
     
   return (
     <Container className="mt-5">
-      <h2>Make Graph <InfoModal/></h2>
-      <MakeGraphForm/>
+      <h2>Make Graph <InfoModal/></h2>      
+      <MakeGraphForm handler={handleSubmit}/>
     </Container>
   )
 }
