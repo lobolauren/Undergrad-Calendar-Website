529660ef9843aaf68375ea018e46808510c90e51
pbaker05 <pbaker05@uoguelpgh.ca>
2022-03-16 12:07:20 -0400

    removed print statements

--- api/course_search.py
+++ api/course_search.py
@@ -27,11 +27,13 @@ def get_courses(name, code, weight, term):
     #get all courses
     coursedata = get_course_data(COURSE_INFO_JSON)
     courseList = []
-
-    attr = get_course_attr(code)
-    num = get_course_number(code)
+    
+    print(name + " " + code + " " + weight + " " + term + ":")
 
     try:
+        attr = get_course_attr(code)
+        num = get_course_number(code)
+
         for dept in coursedata['courses']:
             for course in coursedata['courses'][dept]:
                 check = True
@@ -40,46 +42,51 @@ def get_courses(name, code, weight, term):
                 if name:
                     if name.lower() not in course['name'].lower():
                         check = False
+                        #print("name")
 
                 #code
                 if attr:
                     if get_course_attr(course['code']) not in attr:
                         check = False
+                        #print("attr")
                 if num:
                     if get_course_number(course['code']) not in num:
                         check = False
+                        #print("num")
                 
                 #weight
                 if weight in "0.25":
                     if course['weight'] != 0.25:
                         check = False
+                        #print("weight")
                 elif weight in "0.5":
                     if course['weight'] != 0.5:
                         check = False
+                        #print("weight")
                 elif weight in "0.75":
                     if course['weight'] != 0.75:
                         check = False
+                        #print("weight")
                 elif weight in "1.0":
                     if course['weight'] != 1.0:
                         check = False
+                        #print("weight")
 
                 #term
                 termcheck = False
                 if len(term) == 2:
                     for t in course['terms']:
                         if t.upper() in term[0].upper() or t.upper() in term[1].upper():
-                            print(t.upper())
                             termcheck = True
                 elif len(term) == 1:
                     for t in course['terms']:
                         if t.upper() in term[0].upper():
-                            print("1")
                             termcheck = True
                 else:
-                    print(len(term))
                     termcheck = True
                 
                 if not termcheck:
+                    #print("term")
                     check = False
                     
                 
