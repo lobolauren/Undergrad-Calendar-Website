04d2a8d3ca4f456d3c8c9bf24f841a214d091e01
Nigel Davis <ndavis10@uoguelph.ca>
2022-03-17 13:53:30 -0400

    updated graph with auto layout

--- api/make_graph.py
+++ api/make_graph.py
@@ -5,19 +5,18 @@ import json
 DEBUG = False
 
 NODE_COLORS = {
-    'searched_course': '#ffc107',
-    'same_dept': '#0d6efd',
-    'diff_dept': '#6c757d'
+    'searched_course': '#ffc107',   # main course
+    'same_dept': '#0d6efd',         # course in the same department
+    'diff_dept': '#6c757d'          # course in different department
 }
 
-def add_node(nodes: list, course_code: str, x, y, color):
+def add_node(nodes: list, course_code: str, color):
     course_code = course_code.lower()
     nodes.append({
         'id': course_code.replace('*', ''),
         'data': {
             'label': make_code_valid(course_code).upper().replace('*', ' ')
         },
-        'position': {'x': x, 'y': y},
         'targetPosition': 'left',
         'sourcePosition': 'right',
         'connectable': False,
@@ -26,10 +25,6 @@ def add_node(nodes: list, course_code: str, x, y, color):
             'background': color,
         },
     })
-
-def update_nodes(nodes_data, course, depth):
-    key = course.lower().replace('*', '')
-    nodes_data[key] = depth
 
 def add_edge(edges: list, course: str, prereq: str, animated: bool=False):
     course = course.lower().replace('*', '')
@@ -45,9 +40,9 @@ def add_edge(edges: list, course: str, prereq: str, animated: bool=False):
     })
 
 def get_node_color(code, og_dept, og_code):
-    if make_code_valid(code) == og_code:
+    if make_code_valid(code) == og_code.upper():
         return NODE_COLORS['searched_course']
-    elif get_course_attr(make_code_valid(code)) == og_dept:
+    elif get_course_attr(make_code_valid(code), upper=True) == og_dept:
         return NODE_COLORS['same_dept']
     else:
         return NODE_COLORS['diff_dept']
@@ -57,7 +52,6 @@ def make_course_graph(code):
     data = get_course_data()
 
     nodes = []
-    nodes_data = {}
     edges = []
 
     if not get_course(data, code):
@@ -66,14 +60,12 @@ def make_course_graph(code):
             'edges': edges
         }
 
-    dept = get_course_attr(code)
+    dept = get_course_attr(code).upper()
 
     visited = set()
     q = [code.upper()]
 
-    depth = 0
     while q:
-        depth += 1
         for _ in range(len(q)):
             cur_course_code = q.pop(0)
 
@@ -81,21 +73,18 @@ def make_course_graph(code):
                 continue
 
             cur_course = get_course(data, cur_course_code)
-            update_nodes(nodes_data, cur_course_code, depth)
-            # if DEBUG:
-            #     print(f'{cur_course_code} [{depth}, {count}]')
+            color = get_node_color(cur_course_code, dept, code)
+            add_node(nodes, cur_course_code, color)
 
             for prereq in get_reg_prereqs(cur_course):
                 if get_course_attr(prereq) not in data['courses']:
                     continue
 
                 add_edge(edges, cur_course_code, prereq)
-                # if DEBUG:
-                #     print(f'   {cur_course_code} -> {prereq}')
 
                 if prereq not in visited:
                     visited.add(prereq)
-                q.append(prereq)
+                    q.append(prereq)
 
             for group in get_eq_prereqs(cur_course):
                 for prereq in group:
@@ -103,23 +92,10 @@ def make_course_graph(code):
                         continue
                     
                     add_edge(edges, cur_course_code, prereq, animated=True)
-                    # if DEBUG:
-                    #     print(f'   {cur_course_code} -> {prereq}')
 
                     if prereq not in visited:
                         visited.add(prereq)
-                    q.append(prereq)
-
-    temp = [[] for _ in range(max(nodes_data.values()))]
-    for node, val in nodes_data.items():
-        temp[val-1].append(node)
-
-    nodes = []
-    for i, row in enumerate(temp):
-        for j, course in enumerate(row):
-            color = get_node_color(course, dept, code)
-            y, x = j*100-((len(row)-1)*100*0.5), i*300
-            add_node(nodes, course, x, y, color)
+                        q.append(prereq)
 
     return {
         'nodes': nodes,
--- course-utility/src/pages/Graph.js
+++ course-utility/src/pages/Graph.js
@@ -2,10 +2,47 @@ import { useEffect, useState } from 'react';
 import ReactFlow, { Background } from 'react-flow-renderer';
 import { useParams } from 'react-router-dom';
 import axios from 'axios'
+import dagre from 'dagre';
 
 import NoPage from './NoPage';
 
 import '../styles/graph.css'
+
+const dagreGraph = new dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+const nodeWidth = 120;
+const nodeHeight = 38;
+
+const getLayoutedElements = (nodes, edges, direction = 'TB') => {
+  const isHorizontal = direction === 'LR';
+  dagreGraph.setGraph({ rankdir: direction, ranksep: 150, nodesep: 50 });
+
+  nodes.forEach((node) => {
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+  });
+
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
+
+  dagre.layout(dagreGraph);
+
+  nodes.forEach((node) => {
+    const nodeWithPosition = dagreGraph.node(node.id);
+    node.targetPosition = isHorizontal ? 'left' : 'top';
+    node.sourcePosition = isHorizontal ? 'right' : 'bottom';
+
+    node.position = {
+      x: nodeWithPosition.x - nodeWidth / 2,
+      y: nodeWithPosition.y - nodeHeight / 2,
+    };
+
+    return node;
+  });
+
+  return { nodes, edges };
+};
 
 const Graph = () => {
 
@@ -18,10 +55,17 @@ const Graph = () => {
   useEffect(() => {
     const getGraph = () => {
       axios.get(global.config.base_url + '/graph/course/' + params.code).then((res) => {
-        setNodes(res.data.nodes);
-        setEdges(res.data.edges);
+
+        const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
+          res.data.nodes,
+          res.data.edges,
+          'LR'
+        );
+
+        setNodes([...layoutedNodes]);
+        setEdges([...layoutedEdges]);
         setReceivedRequest(true)
-      })
+      });
     }
     getGraph();
   }, [])
