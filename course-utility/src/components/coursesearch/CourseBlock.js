d9dd7d22dc84ce914f3214c457eeba7ac5d7c21c
ga-holmes <garrett.a.holmes01@gmail.com>
2022-03-17 11:06:16 -0400

    added button to graph prerequisites on coursesearch

--- course-utility/src/components/coursesearch/CourseBlock.js
+++ course-utility/src/components/coursesearch/CourseBlock.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import { Button, Card } from 'react-bootstrap'
 
 const CourseBlock = ({ code, name, term, weight, description }) => {
+
+  let graphLink = '/graph/course/'+code
+
   return (
-    <div className='courseblock'>
-        <span>{code} - {name} [{weight}] : {term}</span>
-        <p>{description}</p>
-    </div>
+        <Card className='courseblock'>
+
+          <Card.Body>
+
+            <Card.Title>{code} - {name} [{weight}] : {term}</Card.Title>
+            <Card.Text>{description}</Card.Text>
+
+            <Button href={graphLink}>Graph Prerequisites</Button>
+          </Card.Body>
+          
+        </Card>
   )
 }
 
