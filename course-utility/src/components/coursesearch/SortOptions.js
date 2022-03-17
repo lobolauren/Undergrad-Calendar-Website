78f120702db711d0b338ec41dcb7f9b9686766bd
Malcolm Wright <mwrigh25@uoguelph.ca>
2022-03-17 08:51:01 -0400

    Fixed formatting of sort options

--- course-utility/src/components/coursesearch/SortOptions.js
+++ course-utility/src/components/coursesearch/SortOptions.js
@@ -1,28 +1,28 @@
 import React from 'react'
-import { Form, Dropdown, Row, Col } from 'react-bootstrap'
+import { Form, Row } from 'react-bootstrap'
 
 const SortOptions = ({sortTypeHandler, sortOrderHandler}) => {
 
     return (
-        <div className="container">
-            <div className='sortOptions mt-5 row'>
-                <Row>
-                    <div className="align-items-center col-2">
-                        <p className="col-2">Sort by</p>
-                    </div>
-
-                    <Form.Select id="typeOption" className="col-6" onChange={sortTypeHandler}>
-                        <option value="courseCodeOption">Course Code</option>
-                        <option value="courseNameOption">Course Name</option>
-                    </Form.Select>
-
-                    <Form.Select id="sortOrder" className="col-6" onChange={sortOrderHandler}>
-                        <option value="ascending">Ascending</option>
-                        <option value="descending">Descending</option>
-                    </Form.Select>
-                </Row>
+        <Row className="mt-3">
+            <div className="align-self-center">
+                <p>Sort by</p>
             </div>
-        </div>
+
+            <div className="col-2">
+                <Form.Select id="typeOption" onChange={sortTypeHandler}>
+                    <option value="courseCodeOption">Course Code</option>
+                    <option value="courseNameOption">Course Name</option>
+                </Form.Select>
+            </div>
+
+            <div className="col-2">
+                <Form.Select id="sortOrder" onChange={sortOrderHandler}>
+                    <option value="ascending">Ascending</option>
+                    <option value="descending">Descending</option>
+                </Form.Select>
+            </div>
+        </Row>
     )
 }
 
