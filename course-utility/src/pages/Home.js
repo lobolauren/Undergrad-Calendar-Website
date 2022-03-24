f03d8ca599c9d64e2d1246ff80c823e0e82b9ff0
Malcolm Wright <mwrigh25@uoguelph.ca>
2022-03-24 11:08:37 -0400

    Removed api call from home

--- course-utility/src/pages/Home.js
+++ course-utility/src/pages/Home.js
@@ -6,30 +6,12 @@ import { useEffect, useState } from "react";
 import { Button } from 'react-bootstrap'
 
 const Home = () => {
-
-    const [data, setData] = useState([]);
-
-    useEffect( () => {
-
-        const fetchData = async () => {
-          axios.get(global.config.base_url + '/').then((res) => {
-            setData(res.data);
-          });
-        }
-        fetchData();
-    }, []);
     
-    function buttonClick() {
-        alert(data)
-    }
-
     return (
         <div className="pageContainer text-center">
             <img src={logo} alt="logo" className="img-fluid" />
 
             <h1>Welcome to University of Guelph Course Utility!</h1>
-	    
-            <Button as="input" type="button" variant="success" value="API Call" onClick={buttonClick}/>
 
         </div>
     )
