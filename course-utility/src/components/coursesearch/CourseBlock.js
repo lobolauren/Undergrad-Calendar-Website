eece0d68f6de3a090a32ca0d2a71ff32c9d40e8d
Malcolm Wright <mwrigh25@uoguelph.ca>
2022-03-29 17:33:48 -0400

    Added prereqs to the courseblock

--- course-utility/src/components/coursesearch/CourseBlock.js
+++ course-utility/src/components/coursesearch/CourseBlock.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
 
-const CourseBlock = ({ code, name, term, weight, description, school }) => {
+const CourseBlock = ({ code, name, term, weight, description, requiredPrereqs, eqPrereqs, school }) => {
   let graphLink = "/graph/" + school + "/course/" + code;
 
   const weightFormat = new Intl.NumberFormat('en-US', { 
     minimumIntegerDigits: 1, 
     minimumFractionDigits: 2 
   }); 
+
+  // Create new string for eq_prereqs in the format: [], [], [] where each square bracket is a group of courses related
+  // either through 1 OF, 1 OR, etc type prerequisites 
+  let newEqPrereqString = "";
+  for (let i = 0; i < eqPrereqs.length; i++) {
+    newEqPrereqString += "[" + eqPrereqs[i] + "]";
+
+    // ensures no comma on last group of eqprereqs
+    if (i < eqPrereqs.length - 1) {
+      newEqPrereqString += ", "
+    }
+  }
 
   return (
     <Card className="courseblock">
@@ -18,6 +30,17 @@ const CourseBlock = ({ code, name, term, weight, description, school }) => {
           [{weightFormat.format(weight)}] : {term.join(", ")}
         </Card.Subtitle>
         <Card.Text>{description}</Card.Text>
+        {/* Only show the pre-reqs if they aren't empty */}
+        {
+          (requiredPrereqs.length !== 0) ?
+          <Card.Subtitle className="mb-2">{"Required prerequisite(s): " + requiredPrereqs}</Card.Subtitle>
+          : ""
+        }
+        {
+          (eqPrereqs.length !== 0) ?
+          <Card.Subtitle className="mb-2">{"1 of/or prerequisite(s): " + newEqPrereqString}</Card.Subtitle>
+          : ""
+        }
 
         <Button href={graphLink}>Graph Prerequisites</Button>
       </Card.Body>
@@ -31,6 +54,8 @@ CourseBlock.defaultProps = {
   term: "",
   weight: "",
   description: "",
+  requiredPrereqs: "",
+  eqPrereqs: ""
 };
 
 export default CourseBlock;
